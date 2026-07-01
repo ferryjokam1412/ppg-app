227172818e65c2033a3967ea6c5e3974865aec43
Ferry Ferdiansyah <ferry.atlms@gmail.com>
2026-07-01 11:12:47 +0700

    Update VC

--- apps/admin-portal/vite.config.js
+++ apps/admin-portal/vite.config.js
@@ -5,6 +5,10 @@ import tailwindcss from '@tailwindcss/vite' // <-- Impor di sini
 // https://vite.dev/config/
 export default defineConfig({
   base: '/',
+  build: {
+    // Tambahkan baris ini agar folder asset admin bernama unik dan tidak tabrakan
+    assetsDir: 'admin-assets' 
+  },
   plugins: [
     react(),
     tailwindcss(), // <-- Tambahkan di dalam array plugins
